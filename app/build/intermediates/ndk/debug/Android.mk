be334868120f9f56946f7ff55e81e777fb1e5c7d
spff <spff@bitmessage.ch>
2017-08-16 21:48:20 +0800

    add programmer dvorak layout and move scripts to the correct path

--- app/build/intermediates/ndk/debug/Android.mk
+++ app/build/intermediates/ndk/debug/Android.mk
@@ -4,13 +4,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libjni_pckeyboard
 LOCAL_LDFLAGS := -Wl,--build-id
 LOCAL_SRC_FILES := \
-	/home/uzuki/Projects/hackerskeyboard/app/src/main/jni/Android.mk \
-	/home/uzuki/Projects/hackerskeyboard/app/src/main/jni/Application.mk \
-	/home/uzuki/Projects/hackerskeyboard/app/src/main/jni/jni/org_pocketworkstation_pckeyboard_BinaryDictionary.cpp \
-	/home/uzuki/Projects/hackerskeyboard/app/src/main/jni/src/char_utils.cpp \
-	/home/uzuki/Projects/hackerskeyboard/app/src/main/jni/src/dictionary.cpp \
+	C:\Users\spff\hakerskeyboard\hackerskeyboard\app\src\main\jni\Android.mk \
+	C:\Users\spff\hakerskeyboard\hackerskeyboard\app\src\main\jni\Application.mk \
+	C:\Users\spff\hakerskeyboard\hackerskeyboard\app\src\main\jni\jni\org_pocketworkstation_pckeyboard_BinaryDictionary.cpp \
+	C:\Users\spff\hakerskeyboard\hackerskeyboard\app\src\main\jni\src\char_utils.cpp \
+	C:\Users\spff\hakerskeyboard\hackerskeyboard\app\src\main\jni\src\dictionary.cpp \
 
-LOCAL_C_INCLUDES += /home/uzuki/Projects/hackerskeyboard/app/src/main/jni
-LOCAL_C_INCLUDES += /home/uzuki/Projects/hackerskeyboard/app/src/debug/jni
+LOCAL_C_INCLUDES += C:\Users\spff\hakerskeyboard\hackerskeyboard\app\src\main\jni
+LOCAL_C_INCLUDES += C:\Users\spff\hakerskeyboard\hackerskeyboard\app\src\debug\jni
 
 include $(BUILD_SHARED_LIBRARY)
